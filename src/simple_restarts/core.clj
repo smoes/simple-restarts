7229215b841d5e36c3ec3d3adb813fc3c623a3c3
Simon Härer <simon.haerer@active-group.de>
2020-06-03 09:15:06 +0200

    Refactor code, add first test

--- src/simple_restarts/core.clj
+++ src/simple_restarts/core.clj
@@ -6,71 +6,56 @@
 
 
 (rec/define-record-type Restart
-  (make-restart n foo) restart?
+  (restart n invocation-function) restart?
   [n restart-name
-   foo restart-foo])
+   invocation-function restart-invocation-function])
 
 
-(rec/define-record-type RestartInvokation
-  (make-restart-invokation n params) restart-invokation?
-  [n restart-invokation-name
-   params restart-invokation-params])
+(rec/define-record-type RestartInvocation
+  (make-restart-invocation n params) restart-invocation?
+  [n restart-invocation-restart-name
+   params restart-invocation-params])
+
+
+(defn invoke-restart [sym & params]
+  (make-restart-invocation sym params))
 
 
 (rec/define-record-type Condition
-  (make-condition n params) condition?
-  [n condition-name
+  (make-condition identifier params) condition?
+  [identifier condition-identifier
    params condition-params])
 
 
-(defmacro defcondition [name params]
-  `(defn ~name ~params
-     (make-condition ~name ~params))) ;; FIXME: Get namespace qualified symbol for name
-
-
-
-
-(defn restart [name fun]
-  (make-restart name fun))
+(defmacro defcondition [identifier params]
+  `(defn ~identifier ~params
+     (make-condition ~identifier ~params)))
 
 
 (defmacro handler-bind [bindings & body]
-  (let [bindings-2 (mapv vec (partition 2 bindings))]
-    `(binding [*handlers* (reduce
-                            (fn [acc# [sym# foo#]]
-                              (assoc acc# sym# foo#))
-                            *handlers*
-                            ~bindings-2)]
-      ~@body)))
+  (assert (even? (count bindings)))
+  `(binding [*handlers* (apply assoc *handlers* ~bindings)]
+     ~@body))
 
 
 
-(defn restart-invokation-exception? [e]
-  (let [data (ex-data e)
-        ?restart-invokation (:restart-invokation data)]
-    (restart-invokation? ?restart-invokation)))
+(defn restart-invocation-exception? [e]
+  (= (:type e) :restart-invocation))
 
 (defn find-restart [restarts name]
   (first (filter (fn [restart] (= (restart-name restart) name)) restarts)))
 
 
 (defn restart-case-catch [e restarts]
-  (if (restart-invokation-exception? e)
-    (let [res     (:restart-invokation (ex-data e))
-          name    (restart-invokation-name res)
-          params  (restart-invokation-params res)
+  (if (restart-invocation-exception? e)
+    (let [res     (:restart-invocation (ex-data e))
+          name    (restart-invocation-restart-name res)
+          params  (restart-invocation-params res)
           restart (find-restart restarts name)]
       (if restart
-        (apply (restart-foo restart) params)
+        (apply (restart-invocation-function restart) params)
         (throw e)))
     (throw e)))
-
-
-
-(defn condition-handler [name]
-  (if-let [handler (get *handlers* name)]
-    handler
-    (throw (ex-info "No handler found" {:condition name}))))
 
 
 (defmacro restart-case [body & restarts]
@@ -80,17 +65,21 @@
       (restart-case-catch e# ~(vec restarts)))))
 
 
-(defn invoke-restart [sym & params]
-  (make-restart-invokation sym params))
+(defn condition-handler [name]
+  (if-let [handler (get *handlers* name)]
+    handler
+    (throw (ex-info "No handler found" {:condition name}))))
+
 
 
 (defn fire-condition [condition]
-  (let [condition-name (condition-name condition)
-        handler (condition-handler condition-name)
+  (let [condition-identifier (condition-identifier condition)
+        handler (condition-handler condition-identifier)
         res (apply handler (condition-params condition))]
-    (if (restart-invokation? res)
-      (throw (ex-info "" {:restart-invokation res}))
-      (throw (ex-info "No restart invokation returned" {:handler handler})))))
+    (if (restart-invocation? res)
+      (throw (ex-info "" {:type :restart-invocation
+                          :restart-invocation res}))
+      (throw (ex-info "No restart invocation returned" {:handler handler})))))
 
 
 
--- test/simple_restarts/core_test.clj
+++ test/simple_restarts/core_test.clj
@@ -2,3 +2,16 @@
   (:require [clojure.test :refer :all]
             [simple-restarts.core :refer :all]))
 
+(defcondition my-test-condition [line])
+(defcondition my-test-condition-2 [line])
+
+(deftest handler-binding
+  (handler-bind
+    [my-test-condition   (fn [a] (invoke-restart :restart a))
+     my-test-condition-2 (fn [a] (invoke-restart :restart (* 2 a)))]
+
+    (is (get *handlers* my-test-condition))
+    (is (= ((get *handlers* my-test-condition) 3) (invoke-restart :restart 3)))
+
+    (is (get *handlers* my-test-condition-2))
+    (is (= ((get *handlers* my-test-condition-2) 3) (invoke-restart :restart 6)))))
